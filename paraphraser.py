0dcb0f57c17cc650c929b94bdba48702e9ab1c89
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2021-09-25 22:21:15 +0100

    Update paraphraser.py

--- paraphraser.py
+++ paraphraser.py
@@ -2,8 +2,8 @@
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 import streamlit as st
 
-model = AutoModelForSeq2SeqLM.from_pretrained("ramsrigouthamg/t5_paraphraser")
-tokenizer = AutoTokenizer.from_pretrained("ramsrigouthamg/t5_paraphraser")
+model = AutoModelForSeq2SeqLM.from_pretrained("ramsrigouthamg/t5-large-paraphraser-diverse-high-quality")
+tokenizer = AutoTokenizer.from_pretrained("ramsrigouthamg/t5-large-paraphraser-diverse-high-quality")
 
 import torch
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
