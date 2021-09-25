f715c2b8cf510fefe086e03ee7f6967444090123
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2021-09-25 22:11:00 +0100

    Update paraphraser.py

--- paraphraser.py
+++ paraphraser.py
@@ -15,23 +15,24 @@ st.title('Automatic Paraphraser')
 
 st.subheader('Original sentence:')
 context = st.text_input('Please type the sentence you want to paraphrase here')
-text = "paraphrase: "+context + " </s>"
+if context:
+        text = "paraphrase: "+context + " </s>"
 
-encoding = tokenizer.encode_plus(text,max_length =1000, padding=True, return_tensors="pt")
-input_ids,attention_mask  = encoding["input_ids"].to(device), encoding["attention_mask"].to(device)
+        encoding = tokenizer.encode_plus(text,max_length =1000, padding=True, return_tensors="pt")
+        input_ids,attention_mask  = encoding["input_ids"].to(device),encoding["attention_mask"].to(device)
 
-model.eval()
-beam_outputs = model.generate(
-    input_ids=input_ids,attention_mask=attention_mask,
-    max_length=1000,
-    early_stopping=True,
-    num_beams=15,
-    num_return_sequences=3
+        model.eval()
+        beam_outputs = model.generate(
+            input_ids=input_ids,attention_mask=attention_mask,
+            max_length=1000,
+            early_stopping=True,
+            num_beams=15,
+            num_return_sequences=3
 
-)
+        )
 
-st.subheader('Results:')
-for beam_output in beam_outputs:
-    sent = tokenizer.decode(beam_output, skip_special_tokens=True,clean_up_tokenization_spaces=True)
-    st.write(sent)
+        st.subheader('Results:')
+        for beam_output in beam_outputs:
+            sent = tokenizer.decode(beam_output, skip_special_tokens=True,clean_up_tokenization_spaces=True)
+            st.write(sent)
 
